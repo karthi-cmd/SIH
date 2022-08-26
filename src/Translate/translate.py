350f82090e6cca3f51d84f59d6ff4c4f56b82674
Kuldeep <71772185+neokd@users.noreply.github.com>
2022-08-26 19:18:41 +0530

    cleaning

--- src/Translate/translate.py
+++
@@ -1,6 +0,0 @@
-from deep_translator import GoogleTranslator
-translated = GoogleTranslator(source='english', target='hindi').translate_file('src/data/csv/18-19_modifieddata.csv')
-print(translated)
-f = open('src/data/Hindi_CSV/hindi_random_3.csv','w')
-f.write(translated)
-f.close()
