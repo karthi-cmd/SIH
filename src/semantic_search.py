239aee9b552323b0a09fc6ebfef03eb22ce9f381
karthi-cmd <85666415+karthi-cmd@users.noreply.github.com>
2023-07-05 17:59:52 +0530

    Revert "Added Links"
    This reverts commit dec3825ad463bec24b27f0091c525fb5f866cbfd.

--- src/semantic_search.py
+++ src/semantic_search.py
@@ -1,10 +1,8 @@
 
 
-import string
 from sentence_transformers import SentenceTransformer, util
 import torch
 import pandas  as pd 
-import re
 embedder = SentenceTransformer('all-MiniLM-L6-v2')
 def semmantic(faqslist,user_message):
     for i in range(0,len(faqslist)):
@@ -33,11 +31,5 @@ def semmantic(faqslist,user_message):
   
 
     for score, idx in zip(top_results[0], top_results[1]):
-        # print(corpus[idx])
-        urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+', corpus[idx])
-        if urls == []:
-            return corpus[idx]
-        else:
-            url = '<a target="_blank" href = "  '+urls[0] + '" ' + ' ">' + urls[0] +'</a>'
-            ans = corpus[idx].replace(urls[0],url)
-            return ans
+        print(corpus[idx])
+        return corpus[idx]
